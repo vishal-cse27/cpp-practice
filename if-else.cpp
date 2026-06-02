46b21feef2d4609b76b8dbe63dfb2e513653c3c4
Vishal Gupta <vgupata48@gmail.com>
2026-06-02 19:00:33 +0530

    Create if-else.cpp

---
+++ if-else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(num % 2 == 0)
+        cout << "Even Number";
+    else
+        cout << "Odd Number";
+
+    return 0;
+}
