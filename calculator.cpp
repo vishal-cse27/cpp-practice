30ec19867c1debaa8d1b3543c0ad2e1dd4f21c48
Vishal Gupta <vgupata48@gmail.com>
2026-06-05 18:07:20 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char op;
+
+    cout << "Enter: ";
+    cin >> a >> op >> b;
+
+    if(op == '+') cout << a + b;
+    else if(op == '-') cout << a - b;
+    else if(op == '*') cout << a * b;
+    else if(op == '/') cout << a / b;
+
+    return 0;
+}
